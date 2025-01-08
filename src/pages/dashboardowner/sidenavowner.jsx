ac29776c94845cff421da4e9dc36056f3c123fb9
diannsryh <suryasihdian@gmail.com>
2025-01-08 17:51:27 +0800

    haduhhhh
    pengen tidurrr

--- src/pages/dashboardowner/sidenavowner.jsx
+++ src/pages/dashboardowner/sidenavowner.jsx
@@ -122,15 +122,6 @@ const SideNavOwner = () => {
                   width: "220px",
                 },
               },
-              {
-                key: "/payment",
-                icon: <CreditCardOutlined />,
-                label: <Link to="/payment">Payment</Link>,
-                style: {
-                  ...getMenuItemStyle("/payment"),
-                  width: "220px",
-                },
-              },
             ],
           },
           {
